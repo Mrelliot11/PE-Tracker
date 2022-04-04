4ae7426a48f791af5cbecdb79f2e279cc297d419
ElliotDubrow <emn2345@gmail.com>
2022-04-04 18:49:45 -0500

    test response for / route

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
-const { connectionString } = require('pg/lib/defaults');
+
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -10,3 +11,17 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
+
+express().use(express.static(path.join(__dirname, 'public'))).use(express.json()).use(express.urlencoded({ extended: true})).set('views', path.join(__dirname, 'views')).set('view engine', 'ejs').get('/', async(req, res) => {
+  
+  try {
+    const client = await pool.connect();
+
+    client.release();
+    
+  } catch (err) {
+    console.error(err);
+    res.send("Error" + err);
+  }
+})
+.listen(PORT, () => console.log('listening on ${ PORT }'));
