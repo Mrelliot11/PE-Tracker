b4547028862f5111329962ee4cc578e4653c38f3
ElliotDubrow <emn2345@gmail.com>
2022-04-04 21:22:35 -0500

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const req = require('express/lib/request');
 const path = require('path');
-const PORT = process.env.PORT || 5432; 
+const PORT = process.env.PORT || 5000; 
 const { Pool } = require('pg');
 
 
