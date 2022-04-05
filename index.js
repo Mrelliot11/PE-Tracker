1e912602453c85b254b165695a70c74d8abe497e
ElliotDubrow <emn2345@gmail.com>
2022-04-04 22:36:35 -0500

    table create change

--- index.js
+++ index.js
@@ -29,16 +29,51 @@ express().use(express.static(path.join(__dirname, 'public'))).use(express.json()
 .get('/db-info', async(req,res) => {
   try {
     const client = await pool.connect();
-    const tables = await client.query(
-      `SELECT c.relname AS table, a.attname AS column, t.typname AS type
-      FROM pg_catalog.pg_class as c
-      LEFT JOIN pg_catalog.pg_attribute as a
-      ON c.oid = a.attrelid AND a.attnum > 0
-      LEFT JOIN pg_catalog.pg_type as t
-      on a.atttypid = t.oid
-      WHERE c.relname in ('users', 'observations', 'students', 'schools', 'tasks')
-      ORDER BY c.relname, a.attnum;`
+      const tables = await client.query(
+`
+  CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email text NOT NULL,
+  password text NOT NULL
+  );
+  
+  CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  school INT NOT NULL,
+  expires DATE NOT NULL
+  );
+  
+  CREATE TABLE schools (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  address TEXT NOT NULL
+  );
+  
+  CREATE TABLE observations (
+  id SERIAL PRIMARY KEY,
+  users_id INT NOT NULL,
+  students_id INT NOT NULL,
+  tasks_id INT NOT NULL,
+  duration INTERVAL NOT NULL
+  );
+  
+  CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  ) 
+  
+  SELECT c.relname AS table, a.attname AS column, t.typname AS type
+  FROM pg_catalog.pg_class as c
+  LEFT JOIN pg_catalog.pg_attribute as a
+  ON c.oid = a.attrelid AND a.attnum > 0
+  LEFT JOIN pg_catalog.pg_type as t
+  on a.atttypid = t.oid
+  WHERE c.relname in ('users', 'observations', 'students', 'schools', 'tasks')
+  ORDER BY c.relname, a.attnum;`
+
     );
+
+    
 
     const locals = {
       'tables': (tables) ? tables.rows : null
